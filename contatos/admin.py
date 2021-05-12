70ebba0e90a900ef97d5b3f1a6d8560333172cdf
Diego Gregório <dijesusgregorio@gmail.com>
2021-05-11 21:50:54 -0300

    criação do ver contato

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contatos.models import Contato, Categoria
 
-# Register your models here.
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome','sobrenome', 'telefone', 'email', 'data_criacao','categoria')
+    list_display_links = ['nome', 'sobrenome']
+    list_per_page = 10
+    search_fields = ['id','nome', 'sobrenome']
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('<int:contato_id>', views.ver_contato, name = 'ver_contato')
 ]
